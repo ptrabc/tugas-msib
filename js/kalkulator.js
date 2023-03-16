cd13e8e142440182f0fb7d8ea732da3c9f94effa
Ramadhan Saputra <126634926+ptrabc@users.noreply.github.com>
2023-03-16 07:01:31 +0700

    Add files via upload

---
+++ js/kalkulator.js
@@ -0,0 +1,62 @@
+function tambah(){
+    var frm = document.getElementById('kalkulator');
+    var a1 = parseFloat(frm.angka.value);
+    var a2 = parseFloat(frm.angka2.value);
+    var total = a1 + a2;
+    frm.hasil.value = total;
+}
+
+function kurang(){
+    var frm = document.getElementById('kalkulator');
+    var a1 = parseFloat(frm.angka.value);
+    var a2 = parseFloat(frm.angka2.value);
+    var total = a1 - a2;
+    frm.hasil.value = total;
+}
+
+function bagi(){
+    var frm = document.getElementById('kalkulator');
+    var a1 = parseFloat(frm.angka.value);
+    var a2 = parseFloat(frm.angka2.value);
+    var total = a1 / a2;
+    frm.hasil.value = total;
+}
+
+function kali(){
+    var frm = document.getElementById('kalkulator');
+    var a1 = parseFloat(frm.angka.value);
+    var a2 = parseFloat(frm.angka2.value);
+    var total = a1 * a2;
+    frm.hasil.value = total;
+}
+
+function pangkat(){
+    var frm = document.getElementById('kalkulator');
+    var a1 = parseFloat(frm.angka.value);
+    var a2 = parseFloat(frm.angka2.value);
+    var total = a1 ** a2;
+    frm.hasil.value = total;
+}
+
+function validation() {
+    var frm = document.getElementById("fname").value;
+    console.log(frm);
+    if (frm == "NaN")
+    {
+      alert("Maaf angka belum terisi");
+    }
+  }
+  
+
+/*tugas ke 4 Javascript
+1. Lengkapilah form dari latihan 15js menggunakan CSS dan table 
+2. Perbagus tampilannya 
+3. Lengkapi perhitungannya dengan:
+- pembagian
+- perkalian
+- pangkat
+4. jika form input dari angka 1 dan 2 kosong dan user klik button, 
+maka tampilkan "Maaf angka belum terisi"
+
+
+*/
